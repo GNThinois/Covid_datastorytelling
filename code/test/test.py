9262f106aeeb8f49bd4416b39764e0bd0867659d
GNThinois <guillaume.nguyenthi@gmail.com>
2022-12-08 16:49:22 +0100

    dataload

--- code/test/test.py
+++ code/test/test.py
@@ -1,9 +1,8 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
-
-df
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+st.table(dataframe)
